645c4ec17a186983cb89e1391af431bdfb5d0f02
kickbell <dev.kickbell@gmail.com>
2022-11-23 23:47:29 +0900

    4

--- DesiginPattern/Decorator Pattern/Decorator Pattern/Beverage.swift
+++ DesiginPattern/Decorator Pattern/Decorator Pattern/Beverage.swift
@@ -7,113 +7,70 @@
 
 import Foundation
 
-//음료
-protocol Beverage {
-    var description: String { get }
+class Beverage {
+    private var description: String
+    private var milk: Bool = false
+    private var soy: Bool = false
+    private var mocha: Bool = false
+    private var whip: Bool = false
     
-    func getDescription() -> String
-    func cost() -> Double
+    init(description: String) {
+        self.description = description
+    }
+    
+    func getDescription() -> String {
+        self.description
+    }
+    
+    private func hasMilk() -> Bool { self.milk }
+    private func hasSoy() -> Bool { self.soy }
+    private func hasMocha() -> Bool { self.mocha }
+    private func hasWhip() -> Bool { self.whip }
+    
+    func setMilk(_ b: Bool) { self.milk = b }
+    func setSoy(_ b: Bool) { self.soy = b }
+    func setMocha(_ b: Bool) { self.mocha = b }
+    func setWhip(_ b: Bool) { self.whip = b }
+    func cost() -> Double {
+        var condimentCost: Double = 0.0 //조미료 비용
+        let milkCost: Double = 2.0
+        let soyCost: Double = 3.0
+        let mochaCost: Double = 2.5
+        let whipCost: Double = 1.0
+        
+        if hasMilk() { condimentCost += milkCost }
+        if hasSoy() { condimentCost += soyCost }
+        if hasMocha() { condimentCost += mochaCost }
+        if hasWhip() { condimentCost += whipCost }
+        
+        return condimentCost
+    }
+    
 }
 
 class HouseBlend: Beverage {
-    let description: String = "하우스 블렌드 원두"
-    
-    func getDescription() -> String {
-        return description
-    }
-    
-    func cost() -> Double {
-        return 2.49
+    override func cost() -> Double {
+        return 2.49 + super.cost()
     }
 }
 
 class DarkRoast: Beverage {
-    let description: String = "다크 로스트 원두"
-    
-    func getDescription() -> String {
-        return description
-    }
-    
-    func cost() -> Double {
-        return 2.49
+    override func cost() -> Double {
+        return 2.49 + super.cost()
     }
 }
 
 class Decaf: Beverage {
-    let description: String = "디카페인"
-    
-    func getDescription() -> String {
-        return description
-    }
-    
-    func cost() -> Double {
-        return 2.29
+    override func cost() -> Double {
+        return 2.29 + super.cost()
     }
 }
 
 class Espresso: Beverage {
-    let description: String = "에스프레소"
-    
-    func getDescription() -> String {
-        return description
-    }
-    
-    func cost() -> Double {
-        return 1.99
+    override func cost() -> Double {
+        return 1.99 + super.cost()
     }
 }
-
-/*
- milk 우유
- steamed milk 데운 우유
- soy milk 두유
- whip 휘핑크림
- mocha 모카
- ...
- */
-class HouseBlendWithMilk: Beverage { ... }
-class HouseBlendWithSteamedMilk: Beverage { ... }
-class HouseBlendWithSoy: Beverage { ... }
-class HouseBlendWithMocha: Beverage { ... }
-class HouseBlendWithWhip: Beverage { ... }
-class HouseBlendWithMilkandSoy: Beverage { ... }
-class HouseBlendWithSteamedMilkandWhip: Beverage { ... }
-class HouseBlendWithSoyandWhip: Beverage { ... }
-class HouseBlendWithMochaandWhip: Beverage { ... }
-class HouseBlendWithWhipandMilkandSoy: Beverage { ... }
-
-class DarkRoastWithMilk: Beverage { ... }
-class DarkRoastWithSteamedMilk: Beverage { ... }
-class DarkRoastWithSoy: Beverage { ... }
-class DarkRoastWithMocha: Beverage { ... }
-class DarkRoastWithWhip: Beverage { ... }
-class DarkRoastWithMilkandSoy: Beverage { ... }
-class DarkRoastWithSteamedMilkandWhip: Beverage { ... }
-class DarkRoastWithSoyandWhip: Beverage { ... }
-class DarkRoastWithMochaandWhip: Beverage { ... }
-class DarkRoastWithWhipandMilkandSoy: Beverage { ... }
-
-class EspressoWithMilk: Beverage { ... }
-class EspressoWithSteamedMilk: Beverage { ... }
-class EspressoWithSoy: Beverage { ... }
-class EspressoWithMocha: Beverage { ... }
-class EspressoWithWhip: Beverage { ... }
-class EspressoWithMilkandSoy: Beverage { ... }
-class EspressoWithSteamedMilkandWhip: Beverage { ... }
-class EspressoWithSoyandWhip: Beverage { ... }
-class EspressoWithMochaandWhip: Beverage { ... }
-class EspressoWithWhipandMilkandSoy: Beverage { ... }
-
-class DecafWithMilk: Beverage { ... }
-class DecafWithSteamedMilk: Beverage { ... }
-class DecafWithSoy: Beverage { ... }
-class DecafWithMocha: Beverage { ... }
-class DecafWithWhip: Beverage { ... }
-class DecafWithMilkandSoy: Beverage { ... }
-class DecafWithSteamedMilkandWhip: Beverage { ... }
-class DecafWithSoyandWhip: Beverage { ... }
-class DecafWithMochaandWhip: Beverage { ... }
-class DecafWithWhipandMilkandSoy: Beverage { ... }
 
 
 
--- DesiginPattern/Decorator Pattern/Decorator Pattern/main.swift
+++ DesiginPattern/Decorator Pattern/Decorator Pattern/main.swift
@@ -7,10 +7,17 @@
 
 import Foundation
 
+let cafelatte = Espresso(description: "카페라떼")
+cafelatte.setMilk(true)
 
+//에스프레소 1.99 + 우유 2.0
+print(cafelatte.getDescription(), cafelatte.cost())
 
+let cafeMocha = Espresso(description: "카페모카")
+cafeMocha.setMocha(true)
+cafeMocha.setWhip(true)
 
-
-let hos = HouseBlend()
+//에스프레소 1.99 + 모카 2.5 + whip 1.0
+print(cafeMocha.getDescription(), cafeMocha.cost())
 
 
