9877477678eb2b4d50cc9135025b41918bd47220
kickbell <dev.kickbell@gmail.com>
2022-11-27 04:07:36 +0900

    6

--- DesiginPattern/FactoryPattern/FactoryPattern/main.swift
+++ DesiginPattern/FactoryPattern/FactoryPattern/main.swift
@@ -15,5 +15,31 @@ print("에단이 주문한 \(pizza.getName())")
 
 pizza = chicagoStore.orderPizza("cheese")
 print("조엘이 주문한 \(pizza.getName())")
+/*
+ ===== Order =====
+ 준비 중: FactoryPattern.NYStyleVeggiePizza
+ 도우를 돌리는 중...
+ 소스를 뿌리는 중...
+ 토핑를 올리는 중...
+  소시지
+ 175도에서 25분 간 굽기
+ 피자를 사선으로 자르기
+ 상자에 피자 담기
+ 에단이 주문한 FactoryPattern.NYStyleVeggiePizza
 
+
+ ===== Order =====
+ 준비 중: FactoryPattern.ChicagoStyleCheesePizza
+ 도우를 돌리는 중...
+ 소스를 뿌리는 중...
+ 토핑를 올리는 중...
+  모짜렐라 치즈
+  감자
+  새우
+ 175도에서 25분 간 굽기
+ 네모난 모양으로 자르기
+ 상자에 피자 담기
+ 조엘이 주문한 FactoryPattern.ChicagoStyleCheesePizza
+ Program ended with exit code: 0
+ */
 
