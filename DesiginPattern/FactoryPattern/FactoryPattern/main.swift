cb85e95fd80d831e08a0883bcd6f9c63b4382184
kickbell <dev.kickbell@gmail.com>
2022-11-27 02:04:50 +0900

    5

--- DesiginPattern/FactoryPattern/FactoryPattern/main.swift
+++ DesiginPattern/FactoryPattern/FactoryPattern/main.swift
@@ -7,20 +7,13 @@
 
 import Foundation
 
-//let pizzaStore = PizzaStore(NYPizzaFactory())
-//let pizza = pizzaStore.orderPizza("veggie")
-//print(pizza.getName())
-
-
 let nyStore = NYPizzaStore()
 let chicagoStore = ChicagoPizzaStore()
-
 
 var pizza = nyStore.orderPizza("veggie")
 print("에단이 주문한 \(pizza.getName())")
 
-
-pizza = chicagoStore.orderPizza("veggie")
+pizza = chicagoStore.orderPizza("cheese")
 print("조엘이 주문한 \(pizza.getName())")
 
 
