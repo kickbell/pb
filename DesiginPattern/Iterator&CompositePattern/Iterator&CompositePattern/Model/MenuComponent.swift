ab7ac02000ba82960bfe56c1fd6d1b54c1f9e170
kickbell <dev.kickbell@gmail.com>
2022-12-14 07:01:02 +0900

    6

--- DesiginPattern/Iterator&CompositePattern/Iterator&CompositePattern/Model/MenuComponent.swift
+++ DesiginPattern/Iterator&CompositePattern/Iterator&CompositePattern/Model/MenuComponent.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 protocol MenuComponent {
+    var name: String { get }
+    var description: String { get }
+    var vegetarian: Bool { get }
+    var price: Double { get }
+    
     func add(_ menuComponent: MenuComponent)
     func remove(_ menuComponent: MenuComponent)
     func getChild(_ i: Int) -> MenuComponent?
@@ -18,6 +23,11 @@ protocol MenuComponent {
  에러를 던져서 thorws로 처리를 해도 되긴 하다.
  */
 extension MenuComponent {
+    var name: String { "" }
+    var description: String { "" }
+    var vegetarian: Bool { false }
+    var price: Double { 0.0 }
+    
     func add(_ menuComponent: MenuComponent) { }
     func remove(_ menuComponent: MenuComponent)  { }
     func getChild(_ i: Int) -> MenuComponent? { nil }
